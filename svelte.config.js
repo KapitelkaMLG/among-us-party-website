94c541c4222d4fb6507405c5782597514d399336
kapitelkamlg <kapitelkamlg@gmail.com>
2023-10-19 01:14:02 +0300

    Set up GH pages

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,10 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ?
-				'sveltekit-github-pages' : '',
+				'/among-us-party-website' : '',
 		}
 	}
 };
